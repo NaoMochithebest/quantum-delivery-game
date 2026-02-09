aaf4bb01555f525922bd62a1c4fcf3089d08e80b
Naothebest <naoaki.mochidajp@gmail.com>
2026-02-09 09:34:44 +0900

    Update symbols.py

--- api/tytan_lite/symbols.py
+++ api/tytan_lite/symbols.py
@@ -1,16 +1,7 @@
-"""Symbolic variable creation compatible with TYTAN API."""
-
 import numpy as np
 from sympy import Symbol
 
-
 def symbols_list(shape, fmt):
-    """
-    Create an ndarray of sympy Symbols.
-    
-    Usage: q = symbols_list([N, N], 'q{}_{}')
-    Returns: numpy ndarray of Symbol objects
-    """
     if len(shape) == 1:
         return np.array([Symbol(fmt.format(i)) for i in range(shape[0])])
     elif len(shape) == 2:
@@ -19,5 +10,4 @@ def symbols_list(shape, fmt):
             for j in range(shape[1]):
                 arr[i][j] = Symbol(fmt.format(i, j))
         return arr
-    else:
-        raise ValueError("Only 1D and 2D shapes are supported")
+    raise ValueError("Only 1D/2D supported")
