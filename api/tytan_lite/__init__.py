5f0ba8e599337f0beee9e8ea52a85a6548db877a
Naothebest <naoaki.mochidajp@gmail.com>
2026-02-10 14:31:30 +0900

    Delete api directory

--- api/tytan_lite/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-tytan_lite: Minimal TYTAN SDK compatible module for QUBO/TSP.
-Original: https://github.com/tytansdk/tytan (Apache-2.0)
-"""
-from .symbols import symbols_list
-from .compile import Compile
-from .auto_array import Auto_array
-from . import sampler
-__all__ = ['symbols_list', 'Compile', 'Auto_array', 'sampler']
